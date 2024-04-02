8975dcf58fe271f8040ac7d4bd8b23800a2ed209
KangHyun1206 <kangh1206@naver.com>
2024-04-03 01:32:06 +0900

    upload_strengthGame

---
+++ strengthGameProject/strengthGame/strengthGame/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace strengthGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusText = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Item = new System.Windows.Forms.Button();
+            this.moneyQuan = new System.Windows.Forms.Label();
+            this.itemQuan = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.StrcostText = new System.Windows.Forms.Label();
+            this.sellCostText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusText
+            // 
+            this.statusText.AutoSize = true;
+            this.statusText.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.statusText.Location = new System.Drawing.Point(278, 40);
+            this.statusText.Name = "statusText";
+            this.statusText.Size = new System.Drawing.Size(112, 22);
+            this.statusText.TabIndex = 0;
+            this.statusText.Text = "게임 시작!";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(208, 266);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "강화!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::strengthGame.Properties.Resources.다운로드;
+            this.pictureBox1.Location = new System.Drawing.Point(208, 98);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 151);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Item
+            // 
+            this.btn_Item.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_Item.Location = new System.Drawing.Point(380, 266);
+            this.btn_Item.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Item.Name = "btn_Item";
+            this.btn_Item.Size = new System.Drawing.Size(172, 50);
+            this.btn_Item.TabIndex = 3;
+            this.btn_Item.Text = "아이템 강화!";
+            this.btn_Item.UseVisualStyleBackColor = true;
+            this.btn_Item.Click += new System.EventHandler(this.btn_Item_Click);
+            // 
+            // moneyQuan
+            // 
+            this.moneyQuan.AutoSize = true;
+            this.moneyQuan.Location = new System.Drawing.Point(37, 24);
+            this.moneyQuan.Name = "moneyQuan";
+            this.moneyQuan.Size = new System.Drawing.Size(59, 12);
+            this.moneyQuan.TabIndex = 4;
+            this.moneyQuan.Text = "돈 : 10000";
+            // 
+            // itemQuan
+            // 
+            this.itemQuan.AutoSize = true;
+            this.itemQuan.Location = new System.Drawing.Point(129, 24);
+            this.itemQuan.Name = "itemQuan";
+            this.itemQuan.Size = new System.Drawing.Size(59, 12);
+            this.itemQuan.TabIndex = 4;
+            this.itemQuan.Text = "아이템 : 1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(565, 266);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "판매";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // StrcostText
+            // 
+            this.StrcostText.AutoSize = true;
+            this.StrcostText.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.StrcostText.Location = new System.Drawing.Point(12, 98);
+            this.StrcostText.Name = "StrcostText";
+            this.StrcostText.Size = new System.Drawing.Size(155, 21);
+            this.StrcostText.TabIndex = 6;
+            this.StrcostText.Text = "강화 비용 : 100";
+            // 
+            // sellCostText
+            // 
+            this.sellCostText.AutoSize = true;
+            this.sellCostText.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.sellCostText.Location = new System.Drawing.Point(12, 141);
+            this.sellCostText.Name = "sellCostText";
+            this.sellCostText.Size = new System.Drawing.Size(155, 21);
+            this.sellCostText.TabIndex = 6;
+            this.sellCostText.Text = "판매 비용 : 100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.sellCostText);
+            this.Controls.Add(this.StrcostText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.itemQuan);
+            this.Controls.Add(this.moneyQuan);
+            this.Controls.Add(this.btn_Item);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusText);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Item;
+        private System.Windows.Forms.Label moneyQuan;
+        private System.Windows.Forms.Label itemQuan;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label StrcostText;
+        private System.Windows.Forms.Label sellCostText;
+    }
+}
+
---
+++ strengthGameProject/strengthGame/strengthGame/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace strengthGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int characterLevel = 0; //강화 레벨
+        int gameMoney = 100000; //시작 금액
+        int gameItem = 1; //굉장해엄청나 카드 수
+        int gameItemPlus = 0;
+        int[] strCost = { 100, 500, 1000, 2000, 5000, 7000, 15000, 30000, 50000, 100000 }; //강화 비용
+        int[] sellCost = { 50, 200, 500, 1000, 3000, 15000, 50000, 100000, 150000, 9999999 }; //판매 비용
+        
+        public bool sResult(int perC)
+        {
+            Random rand = new Random(); //랜덤 생성 변수 선언
+            int percent = (int)rand.Next(1, 101); //1~100중 랜덤한 숫자 받아와서 정수형(Int)으로 변환한 값을 저장
+            if(percent<=perC) //확률보다 숫자가 낮으면 강화 성공!
+            {
+                ++characterLevel; //강화 레벨 증가
+                statusText.Text = characterLevel + "강"; //강화 레벨 텍스트 표현
+                return true; //True값을 줘서 실행
+            }
+            else 
+            { 
+                characterLevel = 0; statusText.Text = "태초마을에 오신 것을 환영합니다."; return false;  //false값을 주면 이미지가 사라짐 그냥 일단 테스팅용
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) //강화버튼 클릭시
+        {
+
+            switch (characterLevel) //강화레벨마다 다르게 적용
+            {
+                case 0: //0강 상태
+                    if (gameMoney < strCost[characterLevel]) //재화가 부족할 때
+                    {
+                        MessageBox.Show("파산하셨습니다. 게임을 종료합니다."); //파산 메시지 출력
+                        Application.Exit(); //게임 종료
+                    }
+                    else //재화가 충분할때
+                    {
+                        gameMoney -= strCost[characterLevel]; //플레이어 재화 감소
+                        pictureBox1.Visible = sResult(100); //성공 시 사진 출력값 :true(보이게). 실패는 false
+                    }
+                    break; //마침표
+
+                case 1: //1강 상태
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("파산하셨습니다. 게임을 종료합니다.");
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(99);
+                    }
+                    break;
+                case 2: //2강 상태.. 이하생략
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("파산하셨습니다. 게임을 종료합니다.");
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(95);
+                    }
+                    break;
+                case 3:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("파산하셨습니다. 게임을 종료합니다.");
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(90);
+                    }
+                    break;
+                case 4:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("파산하셨습니다. 게임을 종료합니다.");
+                        Application.Exit();
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(85);
+                    }
+                    break;
+                case 5:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("잔액이 부족합니다! 캐릭터를 판매하세요!"); //이윤을 볼 수 있기 때문에 파산X
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(80);
+                    }
+                    break;
+                case 6:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("잔액이 부족합니다! 캐릭터를 판매하세요!");
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(70);
+                    }
+                    break;
+                case 7:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("잔액이 부족합니다! 캐릭터를 판매하세요!");
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(50);
+                    }
+                    break;
+                case 8:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("잔액이 부족합니다! 캐릭터를 판매하세요!");
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(30);
+                    }
+                    break;
+                case 9:
+                    if (gameMoney < strCost[characterLevel])
+                    {
+                        MessageBox.Show("잔액이 부족합니다! 캐릭터를 판매하세요!");
+                    }
+                    else
+                    {
+                        gameMoney -= strCost[characterLevel];
+                        pictureBox1.Visible = sResult(10);
+                    }
+                    break;
+                case 10:
+                    MessageBox.Show("최고 강화 레벨입니다."); //강화 불가 메시지 출력(최고 레벨)
+                    break;
+            }
+            ++gameItemPlus;
+            if(gameItemPlus == 10) 
+            {
+                gameItemPlus = 0;
+                ++gameItem;
+            }
+            moneyQuan.Text = "돈 : " + gameMoney.ToString(); //남은 재화 출력
+            itemQuan.Text = "아이템 : " + gameItem.ToString(); //남은 아이템 개수 출력
+            StrcostText.Text = "강화 비용 : " + strCost[characterLevel];
+            sellCostText.Text = "판매 비용 : " + sellCost[characterLevel];
+        }
+
+        private void btn_Item_Click(object sender, EventArgs e) //아이템(굉장히 엄청나) 사용 버튼 클릭시
+        {
+            if(gameItem == 0) //아이템이 없을 때
+            {
+                MessageBox.Show("아이템이 없습니다!"); //사용 불가 메시지 출력
+            }
+            else //아이템이 있을 때
+            {
+                --gameItem; //아이템 수 감소
+                ++characterLevel; //강화 레벨 증가
+                statusText.Text = characterLevel + "강"; //강화 레벨 텍스트 출력
+                itemQuan.Text = "아이템 : "+gameItem.ToString(); //아이템 개수 출력
+                pictureBox1.Visible = true; //사진 보여지게 설정
+            }
+            StrcostText.Text = "강화 비용 : " + strCost[characterLevel];
+            sellCostText.Text = "판매 비용 : " + sellCost[characterLevel];
+        }
+
+        private void button2_Click(object sender, EventArgs e) //판매 버튼 클릭시
+        {
+            if(characterLevel == 0) //강화하지 않았을 때. 즉, 강화 레벨이 0일때
+            {
+                MessageBox.Show("판매할 수 없는 강화 단계입니다."); //판매 불가 메시지 출력
+            }
+            else if(characterLevel == 1) //강화 레벨이 1일 때
+            {
+                gameMoney += sellCost[characterLevel-1]; //플레이어 재화 증가(판매 비용 획득)
+                moneyQuan.Text = "돈 : " + gameMoney.ToString(); //재화 텍스트 출력
+                characterLevel = 0; //판매 후 레벨 초기화
+                statusText.Text = characterLevel + "강"; //강화 레벨 텍스트 출력
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. "); //판매 완료 메시지 출력
+                
+            }
+            else if (characterLevel == 2) //강화 레벨이 2일 때.. 이하생략
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 3)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 4)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 5)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 6)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 7)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 8)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 9)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else if (characterLevel == 10)
+            {
+                gameMoney += sellCost[characterLevel - 1];
+                moneyQuan.Text = "돈 : " + gameMoney.ToString();
+                characterLevel = 0;
+                statusText.Text = characterLevel + "강";
+                MessageBox.Show("캐릭터(" + characterLevel + ")를 판매하였습니다. ");
+
+            }
+            else //버그 발생시 대응(혹시나 하고)
+            {
+                MessageBox.Show("오류가 감지되었습니다. 프로그램을 종료합니다.");
+                Application.Exit();
+            }
+            StrcostText.Text = "강화 비용 : " + strCost[characterLevel];
+            sellCostText.Text = "판매 비용 : " + sellCost[characterLevel];
+        }
+    }
+}
