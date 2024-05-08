0a25540c99aaebcb55c92f9b4284491e02389f1e
KangHyun1206 <162896495+KangHyun1206@users.noreply.github.com>
2024-05-08 17:38:34 +0900

    Update_20240508_17:38

---
+++ STGame_Ver.sheetData/GoogleSheetTestingFile/strengthGame/strengthGame/Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace strengthGame
+{
+    public partial class Login : Form
+    {
+        int pwd = 1234;
+        Form1 Form1 = new Form1();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if(pwdBox.Text == "1234")
+            {
+                Form1.UserId = idBox.Text; //아이디명 > Form1 변수(UserId)로 전송
+                MessageBox.Show("로그인 성공!");
+                Form1.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패! (힌트 : 1234)");
+            }
+        }
+    }
+}
